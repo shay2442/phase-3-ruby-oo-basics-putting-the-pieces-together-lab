b8c59e0579617b7b23e04a88919adf619fd3f9e7
shaywacker <shay2442@yahoo.com>
2021-12-08 16:00:37 -0600

    commit tree 1a0a5c5da35f7273cc76e721a290124c97a4f04f

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,11 @@
-
-
+require "pry"
+class Book
+    attr_accessor :author, :page_count, :genre, :turn_page
+    def initialize(title="And Then There Were None", author, page_count, genre, turn_page)
+        @author = "Agatha Christie"
+        @page_count = 272
+        @genre = "Mystery"
+        @turn_page = "Flipping the page...wow, you read fast!"
+    end
+end
+# b1 = Book.new("Agatha Christie", 272, "Mystery", "Flipping the page...wow, you read fast!")
